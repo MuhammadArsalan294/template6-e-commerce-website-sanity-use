987dc81708da65d1bdb2e6ef1b26299afc267c11
unknown <you@example.com>
2025-02-07 07:53:49 -0800

    first commit

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,4 +1,4 @@
-{/* 
+{/*
 import { createClient } from 'next-sanity'
 
 import { apiVersion, dataset, projectId } from '../env'
@@ -9,12 +9,12 @@ export const client = createClient({
   apiVersion,
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
 })
- */}
+*/}
 
 import { createClient } from "next-sanity";
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "", // Ensure env variables exist
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "kbp3arwz", // Ensure env variables exist
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production", // Default to production
   apiVersion: "2023-01-01",
   useCdn: true,
